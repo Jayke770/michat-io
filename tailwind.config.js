0a542fe3441e4916954fa9d8143ffcdbb099a60b
Jayke770 <jakevelaco40@gmail.com>
2021-08-03 07:12:36 +0800

    add tailwindcss config & build script

---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  mode: 'jit',
+  purge: {
+    content: ['./views/**/*.ejs']
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      bluegray: colors.blueGray,
+      coolgray: colors.coolGray,
+      gray: colors.gray,
+      truegray: colors.trueGray,
+      warmgray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose
+    },
+    fontFamily: {
+      'sans': ["Inter var", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'my': {'max': '860px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+      'xsm': {'max': '450px'},
+      'xxsm': {'max': '350px'},
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'class',
+    }),
+  ],
+}
