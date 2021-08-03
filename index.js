1d340a2dcc9234e6a91ab5bcb79755aaeb3808ff
Jayke770 <jakevelaco40@gmail.com>
2021-08-03 10:21:22 +0800

    hmmm

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const mongoose = require('mongoose')
 const users = require('./models/users')
 //connect to mongodb database 
 connectdb()
+console.log("Hmmmmm")
 async function connectdb(){
     console.log("Connecting to database")
     try{
